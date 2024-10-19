f9926a795a9e2e0fedfcabe99a628545b224f55e
daishi <daishi@axlight.com>
2024-10-19 21:56:38 +0900

    fix script

--- src/run-tests.ts
+++ src/run-tests.ts
@@ -3,20 +3,26 @@ import { execSync } from 'node:child_process';
 
 import type { TestCmds } from './define-test.ts';
 
-const jotaiPkg = process.env.JOTAI_PKG || 'jotai';
+const jotaiPkg = process.env.JOTAI_PKG || 'jotai@latest';
 
 rmSync('./build', { recursive: true, force: true });
 mkdirSync('./build');
 
-for (const name of readdirSync('./src/tests')) {
-  if (name.endsWith('.ts')) {
-    const cmds: TestCmds = (await import(`./tests/${name}`)).default;
-    const cwd = `./build/${name.replace(/\.ts$/, '')}`;
+const runCmd = (name: string, cmd: string, cwd: string) => {
+  console.log(`[${name}] Running: ${cmd}`);
+  execSync(cmd, { cwd, encoding: 'utf8' });
+};
+
+for (const file of readdirSync('./src/tests')) {
+  if (file.endsWith('.ts')) {
+    const name = file.replace(/\.ts$/, '');
+    const cmds: TestCmds = (await import(`./tests/${file}`)).default;
+    const cwd = `./build/${name}`;
     mkdirSync(cwd);
-    execSync(cmds.cloneCmd, { cwd, encoding: 'utf8' });
-    execSync(cmds.installCmd, { cwd, encoding: 'utf8' });
-    execSync(cmds.overrideCmd(jotaiPkg), { cwd, encoding: 'utf8' });
-    execSync(cmds.testCmd, { cwd, encoding: 'utf8' });
+    runCmd(name, cmds.cloneCmd, cwd);
+    runCmd(name, cmds.installCmd, cwd);
+    runCmd(name, cmds.overrideCmd(jotaiPkg), cwd);
+    runCmd(name, cmds.testCmd, cwd);
     console.log(`Done: ${name}`);
   }
 }
