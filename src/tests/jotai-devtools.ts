9cfc45c8ffd9b9700f3056d16dab2406e41eed90
Arjun Vegda <14841132+arjunvegda@users.noreply.github.com>
2026-05-05 10:09:47 -0400

    refactor: temporarily point jotai-devtools to rev3 branch (#4)

--- src/tests/jotai-devtools.ts
+++ src/tests/jotai-devtools.ts
@@ -2,7 +2,7 @@ import { defineTest } from '../define-test.ts';
 
 export default defineTest({
   cloneCmd:
-    'curl -L https://github.com/jotaijs/jotai-devtools/archive/HEAD.tar.gz | tar zx --strip-components=1',
+    'curl -L https://github.com/jotaijs/jotai-devtools/archive/refs/heads/refactor/jotai-devtools-rev3.tar.gz | tar zx --strip-components=1',
   installCmd: 'pnpm install',
   overrideCmd: (pkg: string) => ['pnpm', 'add', pkg],
   testCmd: 'pnpm test',
