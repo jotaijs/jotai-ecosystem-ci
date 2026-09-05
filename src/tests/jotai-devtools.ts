5e3947e9c4aee32e911a7f91996f6520335879e8
Arjun <14841132+arjunvegda@users.noreply.github.com>
2026-09-05 10:56:42 -0400

    refactor: temporarily point jotai-devtools to v3 branch

--- src/tests/jotai-devtools.ts
+++ src/tests/jotai-devtools.ts
@@ -2,7 +2,7 @@ import { defineTest } from '../define-test.ts';
 
 export default defineTest({
   cloneCmd:
-    'curl -L https://github.com/jotaijs/jotai-devtools/archive/HEAD.tar.gz | tar zx --strip-components=1',
+    'curl -L https://github.com/jotaijs/jotai-devtools/archive/refs/heads/refactor/feat/add-jotai-v3-support.tar.gz | tar zx --strip-components=1',
   installCmd: 'pnpm install',
   overrideCmd: (pkg: string) => ['pnpm', 'add', pkg],
   testCmd: 'pnpm test',
